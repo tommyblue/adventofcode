4a3e1fa4ebec9036fec303966a408cd4071e3b13
Tommaso Visconti <tommaso.visconti@gmail.com>
2017-01-22 11:12:32 +0100

    day3 part2

--- main.go
+++ main.go
@@ -13,21 +13,42 @@ func check(e error) {
 		panic("Error")
 	}
 }
+
+type triangle struct {
+	vertex []int
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	check(err)
 	scanner := bufio.NewScanner(file)
 	validTriangles := 0
-
+	var tmpTriangles []triangle
+	completed := false
 	for scanner.Scan() {
-		elems := strings.Split(scanner.Text(), " ")
-		var vertex []int
-		for i := 0; i < len(elems); i++ {
-			if s, err := strconv.Atoi(strings.Trim(elems[i], " ")); err == nil {
-				vertex = append(vertex, s)
+		if completed || len(tmpTriangles) == 0 {
+			tmpTriangles = make([]triangle, 3)
+			completed = false
+		}
+		var elems []string
+		tmpElems := strings.Split(scanner.Text(), " ")
+		for i := 0; i < len(tmpElems); i++ {
+			if tmpElems[i] != "" {
+				elems = append(elems, tmpElems[i])
 			}
 		}
-		validTriangles += calculateResult(vertex)
+		for i := 0; i < len(elems); i++ {
+			s, err := strconv.Atoi(strings.Trim(elems[i], " "))
+			if err == nil {
+				tmpTriangles[i].vertex = append(tmpTriangles[i].vertex, s)
+			} else {
+				panic(err)
+			}
+			if len(tmpTriangles[i].vertex) == 3 {
+				validTriangles += calculateResult(tmpTriangles[i].vertex)
+				completed = true
+			}
+		}
 	}
 	fmt.Printf("Found %d valid triangles\n", validTriangles)
 }
